9ed9e8cf5cc2642d3ffcb3dd3977a09f0bcd2ea4
Adrian Lazar <adrian.lazar95@outlook.com>
2019-09-27 15:13:36 +0300

    Added support for AWS Cognito JWT

--- py_jwt_validator/py_jwks/__init__.py
+++ py_jwt_validator/py_jwks/__init__.py
@@ -5,10 +5,13 @@ requests_cache.install_cache(expire_after=cache_time)
 requests_cache.remove_expired_responses()
 
 def get_e_n(kid, issuer):
-    if 'oauth2' in issuer:
-        url = f"{issuer}/v1/keys"
-    else:
-        url = f"{issuer}/oauth2/v1/keys"
+    if 'okta' in issuer:
+        if 'oauth2' in issuer:
+            url = f"{issuer}/v1/keys"
+        else:
+            url = f"{issuer}/oauth2/v1/keys"
+    elif 'amazonaws' in issuer: ## Added AWS Cognito support.
+        url = f"{issuer}/.well-known/jwks.json"
 
     r = requests.get(url)
     keys = r.json()['keys']
