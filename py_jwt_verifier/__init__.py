40f9164e80912ec4ec35c581578820bd0383cf34
Adrian Lazar <adrian-external@adikteev.com>
2024-02-23 19:38:28 +0200

    Black format + Removed redundant comments

--- py_jwt_verifier/__init__.py
+++ py_jwt_verifier/__init__.py
@@ -1,8 +1,9 @@
-######################################################
-# 
-#                 PyJwtException
-#     
-######################################################
+from time import time
+
+from .jwk import JWK
+from .jws import JWS
+from .utils import Utils
+
 
 class PyJwtException(Exception):
     def __init__(self, arg=None):
@@ -21,35 +22,33 @@ class PyJwtException(Exception):
             "ssl": "The SSL Certificate seems to be issued for a different domain or the certificate chain is not present.",
             "cache-store": "Invalid cache store. Please choose between: memory, sqlite, mongo and redis.",
             "cache-lifetime": "Invalid cache lifetime! The lifetime must be higher than 1 and less than 30 days.",
-            "json": "Invalid JSON data. Either the /keys endpoint is not reachable or the response data is invalid."
+            "json": "Invalid JSON data. Either the /keys endpoint is not reachable or the response data is invalid.",
         }
         if arg not in self.params:
             Exception.__init__(self, "General, non-determined error.")
 
         for k, v in self.params.items():
             if k == arg:
-                Exception.__init__(self, f'{v}')
-
-
-######################################################
-# 
-#                 PyJwtVerifier
-#     
-######################################################
-
-from time import time
-
-from .jwk import JWK
-from .jws import JWS
-from .utils import Utils
+                Exception.__init__(self, f"{v}")
 
 
 class PyJwtVerifier:
 
-    def __init__(self, jwt, 
-                 cid=None, aud=None, iss=None, auto_verify=True, check_expiry=True,
-                 cache_enabled=True, cache_lifetime=1, cache_store="sqlite",
-                 cache_store_connection=None, oidc_metadata_url=None, **custom_claims):
+    def __init__(
+        self,
+        jwt,
+        cid=None,
+        aud=None,
+        iss=None,
+        auto_verify=True,
+        check_expiry=True,
+        cache_enabled=True,
+        cache_lifetime=1,
+        cache_store="sqlite",
+        cache_store_connection=None,
+        oidc_metadata_url=None,
+        **custom_claims,
+    ):
 
         self.jwt = jwt.split(".")
         self.is_format_valid()
@@ -57,13 +56,15 @@ class PyJwtVerifier:
         self.header = self.jwt[0]
         self.payload = self.jwt[1]
         self.signature = self.jwt[2]
-        self.message = self.header.encode("ascii") + b"." + self.payload.encode("ascii") ## will be used to obtain the computed hash
+        self.message = (
+            self.header.encode("ascii") + b"." + self.payload.encode("ascii")
+        )  ## will be used to obtain the computed hash
 
         self.utils = Utils()
         self.decoded_jwt = self.utils.b64_decode(self.jwt)
         self.decoded_header = self.decoded_jwt[0]
         self.decoded_payload = self.decoded_jwt[1]
-        
+
         if check_expiry:
             self.is_expired()
 
@@ -71,10 +72,25 @@ class PyJwtVerifier:
         self.aud = aud
         self.iss = iss
         self.custom_claims = custom_claims
-        
-        self.jwk = JWK(PyJwtException, cache_enabled, cache_lifetime, cache_store, cache_store_connection, oidc_metadata_url)
-        self.jws = JWS(PyJwtException, self.decoded_header, self.decoded_payload, self.message, self.signature, self.jwk, self.utils)
-        
+
+        self.jwk = JWK(
+            PyJwtException,
+            cache_enabled,
+            cache_lifetime,
+            cache_store,
+            cache_store_connection,
+            oidc_metadata_url,
+        )
+        self.jws = JWS(
+            PyJwtException,
+            self.decoded_header,
+            self.decoded_payload,
+            self.message,
+            self.signature,
+            self.jwk,
+            self.utils,
+        )
+
         if auto_verify is True:
             self.verify()
 
@@ -88,17 +104,16 @@ class PyJwtVerifier:
         exp = self.decoded_payload.get("exp")
         if exp:
             time_now = int(time())
-            
+
             if time_now >= exp:
                 raise PyJwtException("exp")
 
     def verify(self, get_payload=False):
 
-
-        ### CLAIM VALIDATION. 
+        ### CLAIM VALIDATION.
         if self.cid:
             if self.cid != self.decoded_payload.get("cid"):
-               raise PyJwtException("cid")
+                raise PyJwtException("cid")
         if self.aud:
             if self.aud != self.decoded_payload.get("aud"):
                 raise PyJwtException("aud")
@@ -108,12 +123,12 @@ class PyJwtVerifier:
         if self.custom_claims:
             if not isinstance(self.custom_claims, dict):
                 raise PyJwtException("custom_claims_type")
-            for k, v in self.custom_claims.items(): # k - claim name, v - claim value
+            for k, v in self.custom_claims.items():  # k - claim name, v - claim value
                 if v != self.decoded_payload.get(k):
                     raise PyJwtException("custom_claim")
 
         ### SIGNATURE VALIDATION
-        self.jws.verifier.verify_signature()           
+        self.jws.verifier.verify_signature()
 
         ### OUTPUT
         if get_payload is True:
@@ -121,7 +136,4 @@ class PyJwtVerifier:
 
     def return_payload(self):
 
-        return {
-            "header":self.decoded_header,
-            "payload":self.decoded_payload
-        }
+        return {"header": self.decoded_header, "payload": self.decoded_payload}
